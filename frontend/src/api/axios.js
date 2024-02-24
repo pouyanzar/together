bfca21a7d5fee1dba854165d3636f195b2507dfa
Pouyan <pouyan.zarbafian@hotmail.com>
2024-02-24 11:05:46 -0500

    update server url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:8000/api/v1";
+const BASE_URL = "https://together-server-8glm.onrender.com";
 export default axios.create({
   baseURL: BASE_URL,
 });
